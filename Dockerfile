5ca502bc0fb3bed4cf4138b3262d44ca102e0b91
Xander Moffatt <xmoffatt@instructure.com>
2017-07-07 14:33:56 -0600

    let build.sh run in dockerland for jenkins
    test plan: jenkins builds should suceed and verify commit
    
    Change-Id: Ie6e67b1a782014ff2c7e75ea1e46343884a80e30
    Reviewed-on: https://gerrit.instructure.com/118216
    Reviewed-by: Andrew Huff <ahuff@instructure.com>
    Tested-by: Jenkins
    Product-Review: Xander Moffatt <xmoffatt@instructure.com>
    QA-Review: Xander Moffatt <xmoffatt@instructure.com>

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM instructure/node-passenger:8
+
+ENV APP_HOME /usr/src/app
+
+USER root
+
+RUN mkdir -p $APP_HOME
+COPY package.json yarn.lock $APP_HOME/
+WORKDIR $APP_HOME
+RUN yarn install
+RUN chown -R docker:docker $APP_HOME
+COPY . $APP_HOME
+RUN ["bash", "-c", "shopt -s extglob dotglob\nchown -R docker:docker $APP_HOME/!(node_modules)"]
+
+USER docker
--- build.sh
+++ build.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
+function cleanup() {
+  exit_code=$?
+
+  echo ": Cleaning up"
+  docker-compose stop
+  docker-compose rm -f
+
+  exit $exit_code
+}
+trap cleanup INT TERM EXIT
+
 set -e
 
+echo ": Build and start containers..."
+export COMPOSE_FILE=docker-compose.test.yml
+docker-compose build
+docker-compose up -d
+
+# docker-compose on mac doesn't flush stdout and stderr properly without a tty,
+# so give it one if we're running under one ourselves
+if [ -t 1 ]; then
+  exec_command="docker-compose exec"
+else
+  exec_command="docker-compose exec -T"
+fi
+
 echo ": Run linters and tests..."
-yarn run lint:check
-yarn run coverage:run
-yarn run coverage:check
+$exec_command app yarn run lint:check
+$exec_command app yarn run coverage:run
+$exec_command app yarn run coverage:check
 
 echo ": Publish code coverage..."
-mv coverage/lcov-report/* coverage
+# copy coverage data out of dockerland so jenkins can see it
+docker cp $(docker-compose ps -q app):/usr/src/app/coverage/lcov-report/. coverage
 
 echo ": Build successful!"
