a4a54d6673c0524f246fcd242f4a08003b61a10c
Andrew Huff <ahuff@instructure.com>
2017-07-12 17:33:04 -0600

    small cleanups
    refs CNVS-38189
    
    test plan: specs
    
    Change-Id: Ie7bf5eae197797f0e7c2c5bc295176737d5841e8
    Reviewed-on: https://gerrit.instructure.com/118826
    Tested-by: Jenkins
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    Product-Review: Andrew Huff <ahuff@instructure.com>
    QA-Review: Andrew Huff <ahuff@instructure.com>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM instructure/node-passenger:8
+FROM instructure/node:8
 
 ENV APP_HOME /usr/src/app
 
--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ function cleanup() {
   exit_code=$?
 
   echo ": Cleaning up"
-  docker-compose stop
-  docker-compose rm -f
+  docker-compose down
 
   exit $exit_code
 }
@@ -16,20 +15,22 @@ set -e
 echo ": Build and start containers..."
 export COMPOSE_FILE=docker-compose.test.yml
 docker-compose build
-docker-compose up -d
 
 # docker-compose on mac doesn't flush stdout and stderr properly without a tty,
 # so give it one if we're running under one ourselves
 if [ -t 1 ]; then
-  exec_command="docker-compose exec"
+  DOCKER_COMPOSE_RUN="docker-compose run "
+  DOCKER_EXEC="docker exec -t" # allocate tty
 else
-  exec_command="docker-compose exec -T"
+  DOCKER_COMPOSE_RUN="docker-compose run -T" # disable tty allocation
+  DOCKER_EXEC="docker exec"
 fi
 
 echo ": Run linters and tests..."
-$exec_command app yarn run lint:check
-$exec_command app yarn run coverage:run
-$exec_command app yarn run coverage:check
+$DOCKER_COMPOSE_RUN -d --name work_container app tail -f /dev/null # don't do anything, but don't exit either
+$DOCKER_EXEC work_container yarn run lint:check
+$DOCKER_EXEC work_container yarn run coverage:run
+$DOCKER_EXEC work_container yarn run coverage:check
 
 echo ": Publish code coverage..."
 # copy coverage data out of dockerland so jenkins can see it
