19a3276ba1dad0ff4b4bf2fb340ea9c471e2edd3
Jacob Fugal <jacob@instructure.com>
2018-04-25 13:46:30 -0600

    cache consul responses
    until next set/del or 10 minutes, whichever comes first. prevents making
    a new consul request for the same data on every single access of the
    consul keystore
    
    Change-Id: Ieed43909ef857835edb28c527b95a22f02e2393c
    Reviewed-on: https://gerrit.instructure.com/148124
    Tested-by: Jenkins
    Reviewed-by: Andrew Huff <ahuff@instructure.com>
    Product-Review: Andrew Huff <ahuff@instructure.com>
    QA-Review: Andrew Huff <ahuff@instructure.com>

--- src/consulKVStore.js
+++ src/consulKVStore.js
@@ -10,6 +10,34 @@ const rawDel = promisify(client.kv.del).bind(client.kv);
 const rawSet = promisify(client.kv.set).bind(client.kv);
 const rawGet = promisify(client.kv.get).bind(client.kv);
 
+const CACHE_DURATION = 10 * 60 * 1000; // ten minutes
+const cache = {
+  result: undefined,
+  validUntil: undefined,
+
+  store(result) {
+    const now = new Date();
+    const nowMS = now.getTime();
+    this.result = result;
+    this.validUntil = nowMS + CACHE_DURATION;
+  },
+
+  isExpired() {
+    const now = new Date();
+    const nowMS = now.getTime();
+    return !this.validUntil || this.validUntil < nowMS;
+  },
+
+  clear() {
+    this.result = undefined;
+    this.validUntil = undefined;
+  },
+
+  read() {
+    return this.result;
+  }
+};
+
 exports.delAll = async function delAll(options) {
   const { prefix } = Object.assign(
     { prefix: process.env.CONSUL_JWT_SECRET_PREFIX },
@@ -20,7 +48,9 @@ exports.delAll = async function delAll(options) {
     key: prefix,
     recurse: true
   };
-  return await rawDel(consulOptions);
+  const result = await rawDel(consulOptions);
+  cache.clear();
+  return result;
 };
 
 exports.set = async function set(k, v, options) {
@@ -33,10 +63,12 @@ exports.set = async function set(k, v, options) {
     key: `${prefix}/${k}`,
     value: v
   };
-  return await rawSet(consulOptions);
+  const result = await rawSet(consulOptions);
+  cache.clear();
+  return result;
 };
 
-exports.getAll = async function getAll(options) {
+exports.getAllUncached = async function getAllUncached(options) {
   const { prefix } = Object.assign(
     { prefix: process.env.CONSUL_JWT_SECRET_PREFIX },
     options
@@ -51,4 +83,16 @@ exports.getAll = async function getAll(options) {
     const key = kv.Key.replace(`${prefix}/`, "");
     return { [key]: kv.Value };
   });
+};
+
+exports.getAll = async function getAll(options) {
+  if (options && options.prefix) {
+    // don't cache explicit prefixes
+    return await exports.getAllUncached(options);
+  } else {
+    if (cache.isExpired()) {
+      cache.store(await exports.getAllUncached(options));
+    }
+    return cache.read();
+  }
 };
