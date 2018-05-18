c2fff612033c6f4e324844ca40a11808ccac4a43
Andrew Huff <ahuff@instructure.com>
2018-05-18 16:10:09 -0600

    tweak pre-push script
    a few tweaks to make the pre-push script more user-friendly
    
    test plan: none
    
    Change-Id: Ie1ee912f5f8e2f718d71de6938846e53a45d7925
    Reviewed-on: https://gerrit.instructure.com/167531
    Tested-by: Jenkins
    Reviewed-by: Jacob Fugal <jacob@instructure.com>
    QA-Review: Andrew Huff <ahuff@instructure.com>
    Product-Review: Andrew Huff <ahuff@instructure.com>

--- pre-push.sh
+++ pre-push.sh
@@ -5,17 +5,23 @@
 
 git diff --quiet
 RESULT=$?
-if [ "$RESULT" -eq 1 ]; then
+if [ "$RESULT" -ne 0 ]; then
     echo "You have untracked changes; please commit them and try again"
     exit 1
 fi
 
 echo "Running linter..."
-docker-compose run --rm app yarn run lint:fix || exit 1
+docker-compose run --rm app yarn run lint:fix
+RESULT=$?
+if [ "$RESULT" -ne 0 ]; then
+    echo "The linter is angry; you'll need to manually fix some things before trying again"
+    exit 1
+fi
 
 git diff --quiet
 RESULT=$?
-if [ "$RESULT" -eq 1 ]; then
-    echo "The linter has been appeased; please amend your commit and try again"
+if [ "$RESULT" -ne 0 ]; then
+    echo "The linter has been appeased; please amend your commit and try again; i.e.:"
+    echo "\tgit add -A && git commit --amend --no-edit"
     exit 1
 fi
